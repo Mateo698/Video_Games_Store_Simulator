c71378ee83c5b2a967316d084a68a295038ec2ae
Mateo <mateora73@gmail.com>
2021-09-22 17:59:00 -0500

    Developed shelf

--- src/model/Shelf.java
+++ src/model/Shelf.java
@@ -1,5 +1,80 @@
 package model;
 
-public class Shelf {
+import java.util.ArrayList;
 
+public class Shelf<K,V> {
+	private K key;
+	private V value;
+	private int hashCode = 0;
+	private ArrayList<Videogame> stock;
+	private Shelf<K,V> next;
+	
+	public Shelf(K key, V value, int hasCode) {
+		stock = new ArrayList<Videogame>();
+		this.setKey(key);
+		this.value = value;
+		setHashCode(hasCode);
+	}
+	
+	
+	
+	public boolean hasGame(int gameCode) {
+		boolean found = false;
+		for (int i = 0; i < stock.size(); i++) {
+			if(stock.get(i).getCode() == gameCode) {
+				found = true;
+			}
+		}
+		return found;
+	}
+	
+	public boolean hasStock(int gameCode) {
+		boolean hastock = false;
+		for (int i = 0; i < stock.size() && !hastock; i++) {
+			if(stock.get(i).getCode() == gameCode) {
+				hastock = true;
+			}
+		}
+		return hastock;
+	}
+
+
+
+	public K getKey() {
+		return key;
+	}
+
+
+
+	public void setKey(K key) {
+		this.key = key;
+	}
+
+
+
+	public int getHashCode() {
+		return hashCode;
+	}
+
+
+
+	public void setHashCode(int hashCode) {
+		this.hashCode = hashCode;
+	}
+
+
+
+	public Shelf<K,V> getNext() {
+		return next;
+	}
+
+
+
+	public void setNext(Shelf<K,V> next) {
+		this.next = next;
+	}
+	
+	
+	
+	
 }
--- src/model/Videogame.java
+++ src/model/Videogame.java
@@ -1,5 +1,28 @@
 package model;
 
 public class Videogame {
+	private int quantity;
+	private int code;
+	
+	public Videogame() {
+		
+	}
 
+	public int getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+
+	public int getCode() {
+		return code;
+	}
+
+	public void setCode(int code) {
+		this.code = code;
+	}
+	
+	
 }
