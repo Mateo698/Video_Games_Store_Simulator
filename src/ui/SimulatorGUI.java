4b2fb5c5b831f9a1d748c5328e270721aa3aae6e
Paula Andrea Trujillo <71205932+PaulaTrujillo27@users.noreply.github.com>
2021-09-23 21:15:20 -0500

    MainPane Changed

--- src/ui/SimulatorGUI.java
+++ src/ui/SimulatorGUI.java
@@ -1,5 +1,6 @@
 package ui;
 
+import java.io.File;
 import java.io.IOException;
 
 import javafx.event.ActionEvent;
@@ -7,6 +8,8 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
@@ -14,6 +17,7 @@ public class SimulatorGUI {
 	
 	private Stage mainStage;
 	private Stage popupStage;
+	private String LOGIN_IMAGE_PATH= "data/images/ps4-games.gif";
 
 	public void start() throws IOException {
 		FXMLLoader loader = new FXMLLoader(getClass().getResource("MainScreen.fxml"));
@@ -24,9 +28,16 @@ public class SimulatorGUI {
     	Scene e = new Scene(root);
     	mainStage.setScene(e);
     	mainStage.show();
+    	File f = new File(LOGIN_IMAGE_PATH);
+		Image img = new Image(f.toURI().toString());
+		this.ImageMainScreen.setImage(img);
 		
 		
 	}
+	
+	@FXML
+    private ImageView ImageMainScreen;
+	
 	
 	 @FXML
 	    void mainScreenButton(ActionEvent event) {
