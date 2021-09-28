9dca70b1b9059a6b6de47cc303a83e4848e4c01c
Paula Andrea Trujillo <71205932+PaulaTrujillo27@users.noreply.github.com>
2021-09-28 18:40:47 -0500

    United Case modified

--- test/model/VideoGameSimulationTest.java
+++ test/model/VideoGameSimulationTest.java
@@ -9,32 +9,48 @@ import org.junit.jupiter.api.Test;
 class VideoGameSimulationTest {
 	
 	Store store;
-	Client client;
-	Shelf shelf;
+	Client client1;
+	Client client2;
+	Checker checker1;
+	Checker checker2;
+	Shelf shelf1;
+	Shelf shelf2;
 	Videogame game1, game2, game3, game4, game5;
-	ArrayList<Videogame> games = new ArrayList<>();
+	ArrayList<Videogame> games1 = new ArrayList<>();
+	ArrayList<Videogame> games2 = new ArrayList<>();
 	ArrayList<Videogame> videogames = new ArrayList<>();
 	HashTable<Character, Shelf> hash;
 	
-	public void scenary1() {
+	public void scenary1() throws Exception {
 		store = new Store();
-		//client = new Client(5678, videogames);
-	}
-
-	@Test
-	public void testAddShelf() {
-		scenary1();
 		game1= new Videogame(8765, 1, 34000);
 		game2= new Videogame(32145, 3, 28000);
 		game3= new Videogame(6753, 2, 73000);
 		game4= new Videogame(8900, 6, 50000);
 		game5= new Videogame(1234, 1, 90000);
-		games.add(game1);
-		games.add(game2);
-		games.add(game3);
-		games.add(game4);
-		games.add(game5);
-		//String message = store.addNewUser(user.getNameUser(), user.getPassword());
+		shelf1.getGames().add(game1);
+		shelf1.getGames().add(game2);		
+		shelf2.getGames().add(game3);			
+		shelf2.getGames().add(game4);	
+		shelf2.getGames().add(game5);	
+		hash.insert('A', shelf1);
+		hash.insert('B', shelf2);
+		videogames.add(game1);
+		videogames.add(game3);
+		client1 = new Client("45678", videogames);
+		videogames.clear();
+		videogames.add(game4);
+		videogames.add(game5);
+		client2= new Client("88987", videogames);
+		checker1= new Checker(store);
+		checker2= new Checker(store);
+	}
+
+	@Test
+	public void testAddShelf() {
+		scenary1();
+		
+		String message = store.addNewUser(user.getNameUser(), user.getPassword());
 		String msg = "The person was added succesfully";
 		//assertEquals(message, msg);
 	}
